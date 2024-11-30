c63c56c886aafa83cce3a5586f6bdae7b89083b1
Juan Brero <juaan.b17@hotmail.com>
2024-11-30 01:09:11 -0300

    Conexion entre cliente y switch

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -1,6 +1,14 @@
 package client;
 
 import configLoader.ConfigLoader;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientMain {
 
@@ -9,12 +17,92 @@ public class ClientMain {
     private String rol;
     private final String serverIp = ConfigLoader.get("switch.ip");
     private final String serverPort = ConfigLoader.get("switch.port");
+    private final Scanner sc = new Scanner(System.in);
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    private ClientMain() {
+
+        try {
+            this.socket = new Socket(serverIp, Integer.parseInt(serverPort));
+            this.out = new PrintWriter(socket.getOutputStream(),true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }catch (IOException e) {
+            e.printStackTrace();
+            out.close();
+        }
+
+    }
+
+    private void interactWithServer() {
+        boolean running = true;
+        listDatabases();
+
+        while(running) {
+
+        }
+
+    }
+
+    public void listDatabases() {
+
+        JSONObject requestJson = new JSONObject();
+        requestJson.put("action", "listDatabases");
+
+        JSONObject responseJson = sendRequest(requestJson);
+
+        if (responseJson != null && responseJson.has("databases")) {
+            System.out.println("Databases:");
+            responseJson.getJSONArray("databases").forEach(db -> System.out.println(db.toString()));
+        }
+        else {
+            System.out.println("No Databases available.");
+        }
+    }
+
+    public JSONObject sendRequest(JSONObject requestJson) {
+        JSONObject responseJson = null;
+        StringBuilder responseBuilder = new StringBuilder();
+
+        try {
+            out.println(requestJson.toString());
+            out.println();
+            out.flush();
+            System.out.println("Request sent: " + requestJson);
+
+            String line;
+
+            while ((line = in.readLine()) != null && !line.isEmpty()) {
+                responseBuilder.append(line);
+            }
+
+
+            if (responseBuilder.length() > 0) {
+                responseJson = new JSONObject(responseBuilder.toString());
+                System.out.println("Response from server: " + responseJson);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return responseJson;
+    }
+
+    public static void clear() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+    }
 
     public static void main (String[] args) {
 
+        ClientMain client = new ClientMain();
         System.out.println("\nConnecting to server...");
 
-
+        client.interactWithServer();
 
     }
+
 }
--- src/main/java/configLoader/ConfigLoader.java
+++ src/main/java/configLoader/ConfigLoader.java
@@ -1,5 +1,6 @@
 package configLoader;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -8,10 +9,7 @@ public class ConfigLoader {
     private static Properties properties = new Properties();
 
     static {
-        try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
-            if (input == null) {
-                throw new IllegalStateException("File config.properties not found.");
-            }
+        try (InputStream input = new FileInputStream("src/main/resources/conection/config.properties")) {
             properties.load(input);
         } catch (Exception e) {
             throw new RuntimeException("Error loading config file", e);
